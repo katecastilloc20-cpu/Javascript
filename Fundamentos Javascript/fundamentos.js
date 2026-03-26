736f53f8c20d6c73c0f268e0e9add6c907dc48ef
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-03-25 20:08:56 -0500

    ejemplos

---
+++ Fundamentos Javascript/fundamentos.js
@@ -0,0 +1,31 @@
+// let nombre = prompt('cual es tu nombre');
+// let edad = Number(prompt('cual es tu edad'));
+// let correo = prompt('cual es tu correo');
+
+// console.log('Hola, tu nombre es', nombre, 'tu edad es', edad, 'y tu correo es', correo)
+
+
+
+
+
+// let precio = prompt('Pon tu precio');
+// let porcentaje = Number(prompt('de cuanto quieres tu descuento'));
+
+// let descuento = precio - ((precio * porcentaje )/ 100);
+
+// let precioFinal = precio - descuento;
+
+// console.log('tu precio final con el descuento es de' ,  precioFinal);
+
+
+
+
+
+let kilometro = Number(prompt('dime que numero en km'));
+
+
+let metro = (kilometro * 1000);
+alert('tu conversion en metros es ' + metro);
+
+
+  

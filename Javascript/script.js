a99c45b06f018d0cfc1b2df6dd201ce8fc99c873
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-08 19:25:36 -0500

    ejercicio 2

---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+let palabras = ["casa", "perro", "gato", "árbol", "cielo", "mar", "sol", "luna", "flor", "río"];
+
+let contador = 0;
+
+for (let i=0; i<palabras.length; i++){
+ contador= palabras[i]
+ palabras[i]= contador.length
+
+}
+console.log(palabras)

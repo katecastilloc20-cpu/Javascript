a9d49fd347de207ba9da98c70c40d29adc04637b
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-08 20:20:12 -0500

    ejercicio clase

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,10 +1,36 @@
-let palabras = ["casa", "perro", "gato", "árbol", "cielo", "mar", "sol", "luna", "flor", "río"];
+let palabras = ["casa", "perro", "gato", "arboles", "cielo", "mar", "so", "luna", "flor", "río"];
 
 let contador = 0;
+let larga= palabras[0]
+let corta= palabras[0]
+
+
 
 for (let i=0; i<palabras.length; i++){
  contador= palabras[i]
  palabras[i]= contador.length
 
+console.log("la palabra", contador, "tiene", + contador.length, "letras")
+
+
+ if (palabras[i].length > larga.length){
+    larga = palabras[i];
+
+ } if (palabras[i].length < corta.length){
+    corta = palabras[i];}
+
+   
+
 }
-console.log(palabras)
+
+
+ 
+
+ console.log("palabra mas larga es",  larga)
+
+    console.log("la palabra mas corta es",  corta)
+
+
+
+
+

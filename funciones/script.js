bd2baa5088556f8da96c1b157b04d4c980cff8f7
katerin castillo <katy950315@hotmail.comssh-keygen -t ed25519 -c katy950315@hotmail.com>
2026-04-14 20:39:45 -0500

    funciones

---
+++ funciones/script.js
@@ -0,0 +1,36 @@
+let numeros = [1,2,3,4,5,6,]
+
+// function  multiploss (numeros){
+//     let multiplos = []
+//     for (let i = 0; i < numeros.length; i++) {
+//         multiplos.push(numeros[i]*2);
+//     }
+//     console.log(multiplos)
+// }
+
+
+
+
+function paress (numeros){
+    let pares =[]
+
+    for (let i = 0; i < numeros.length; i++) {
+        if(numeros % 2 === 0){
+            console.log(`${numeros} es par `)
+        }else {
+            console.log(`${numeros} es impar`)
+        }
+        
+    }
+}
+
+
+
+
+function repetir(numeros){
+
+    for (let i = 0; i < 9; i++){
+        console.log(`hola`)
+    }
+
+}
